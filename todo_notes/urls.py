381b34025d488b2d897a3158fee0e9dadf5fb227
Sergei <serguchers@gmail.com>
2021-12-29 15:41:04 +0300

    Lesson_1. Code style refactor

--- todo_notes/urls.py
+++ todo_notes/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from rest_framework.routers import DefaultRouter
+
 from usersapp.views import UsersViewSet
 
 router = DefaultRouter()
-router.register('users', UsersViewSet)
+router.register("users", UsersViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(router.urls)),
 ]
--- usersapp/views.py
+++ usersapp/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+
 from .models import BaseUser
 from .serializers import BaseUserSerializer
+
 
 # Create your views here.
 class UsersViewSet(ModelViewSet):
