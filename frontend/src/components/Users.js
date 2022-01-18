6aa7c4e9d1eaf9b485f736247302a9cf593a5968
Sergei <81819528+Serguchers@users.noreply.github.com>
2022-01-18 15:09:58 +0200

    Lesson 2  (#3)
    * blank
    
    * lesson_2. React components and users.

---
+++ frontend/src/components/Users.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+        <td>
+            {user.username}
+        </td>
+        <td>
+            {user.first_name}
+        </td>
+        <td>
+            {user.last_name}
+        </td>
+    </tr>
+    )
+}
+
+
+const UserList = ({users}) => {
+    return (
+        <table>
+        <th>
+            Username
+        </th>
+        <th>
+            First Name
+        </th>
+        <th>
+            Last Name
+        </th>
+        <tbody>
+        {users.map((user) => <UserItem user={user} />)}
+        </tbody>
+    </table>
+
+    )
+}
+
+export default UserList
--- todo_notes/settings.py
+++ todo_notes/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "usersapp",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -127,3 +129,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "usersapp.BaseUser"
+
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:3000",
+]
